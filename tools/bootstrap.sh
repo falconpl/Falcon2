2d16510a85421411431cb53eb2c575e64eb0f44b
Giancarlo Niccolai <gc@falconpl.org>
2018-01-02 13:48:01 +0000

    Using a simpler policy to bootstrap git

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ##############################################################################
 #   FALCON - The Falcon Programming Language
 #   FILE: bootstrap.sh
@@ -7,13 +7,14 @@
 #   -------------------------------------------------------------------
 #   Author: Giancarlo Niccolai
 #   Begin : Tue, 02 Jan 2018 13:38:35 +0000
-#   Touch : Tue, 02 Jan 2018 13:38:35 +0000
+#   Touch : Tue, 02 Jan 2018 13:48:01 +0000
 #
 #   -------------------------------------------------------------------
 #   (C) Copyright 2018 The Falcon Programming Language
 #   Released under Apache 2.0 License.
 ##############################################################################
 
-git config --local --add core.hooksPath githooks
+SCRIPTPATH="$(cd "$(dirname "$0")"; pwd -P)"
+ln -s ../../githooks/pre-commit "$SCRIPTPATH/../.git/hooks/pre-commit"
 
 
