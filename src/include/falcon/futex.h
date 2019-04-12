156996938c37ca09dddd9b9f6a9c05f4bdd92b8f
Giancarlo Niccolai <gc@falconpl.org>
2019-04-12 22:39:05 +0100

    Added a FUTEX class, with higher lock/synchronization performance with respect to a mutex

--- src/include/falcon/futex.h
+++ src/include/falcon/futex.h
@@ -17,13 +17,28 @@
 #define _FALCON_FUTEX_H_
 
 #include <atomic>
+#include <thread>
+#include <iostream>
+
+
 
 namespace Falcon {
 /**
- * Lightweight non-blocking mutex
+ * Lightweight non-blocking mutex.
+ *
+ * The Spincount template parameter controls the spincount behavior.
+ *
+ * With zero, it's off, and the futex behaves as a pure idle loop/futex.
+ *
+ * With one, the thread yields whenever it detects contention on the
+ * futex.
+ *
+ * With more than one, the a contended lock is idle-spun down to zero,
+ * and if still unsuccessful, the thread is yielded.
  */
+template<unsigned int spinCount=0>
 class Futex {
-   std::atomic<bool> m_status{false};
+   std::atomic<bool> m_owned{false};
 
 public:
    Futex() {}
@@ -32,17 +47,71 @@ public:
    ~Futex() {}
 
    void lock() noexcept {
-      bool locker = false;
-      while(!m_status.compare_exchange_weak(locker, true,
-            std::memory_order_acq_rel, std::memory_order_relaxed));
+      bool isOwned = false;
+      unsigned int sc = spinCount;
+      while(!m_owned.compare_exchange_weak(
+            isOwned, true,
+            std::memory_order_acq_rel,
+            std::memory_order_relaxed) || isOwned ) {
+         isOwned = false;
+
+         if(spinCount && --sc == 0) {
+            std::this_thread::yield();
+         }
+      }
    }
 
    void unlock() noexcept {
-      m_status.store(false, std::memory_order_release);
+      m_owned.store(false, std::memory_order_release);
    }
 
    bool isLocked() noexcept {
-      return m_status.load(std::memory_order_acquire);
+      return m_owned.load(std::memory_order_acquire);
+   }
+};
+
+
+class RFutex {
+   static std::atomic<unsigned int> s_count;
+   static thread_local unsigned int s_thread_id;
+   std::atomic<unsigned int> m_owner{0};
+   unsigned int m_count{0};
+
+public:
+   RFutex() {}
+   RFutex(const RFutex& )= delete;
+   RFutex(RFutex&& )= delete;
+   ~RFutex() {}
+
+   void lock() noexcept {
+      if (s_thread_id == 0) {
+         s_thread_id = ++s_count;
+      }
+
+      unsigned int free = 0;
+      unsigned int thread_id = s_thread_id;
+
+      while(!m_owner.compare_exchange_strong(
+            free, thread_id,
+            std::memory_order_seq_cst,
+            std::memory_order_seq_cst) || free != thread_id) {
+         free = 0;
+      }
+      ++m_count;
+   }
+
+   void unlock() noexcept {
+      if(m_count > 0 && --m_count == 0) {
+         m_owner.store(0, std::memory_order_seq_cst);
+      }
+   }
+
+   bool isLocked() noexcept {
+      return m_owner.load(std::memory_order_seq_cst) != 0;
+   }
+
+   bool isOwner() noexcept {
+      return m_owner.load(std::memory_order_seq_cst) == s_thread_id;
    }
 };
 
--- src/test/engine/futex.fut.cpp
+++ src/test/engine/futex.fut.cpp
@@ -16,13 +16,18 @@
 #include <falcon/fut/fut.h>
 #include <falcon/futex.h>
 #include <mutex>
+#include <thread>
+#include <vector>
 
 using namespace Falcon;
 
 class FutexTest: public Falcon::testing::TestCase
 {
 public:
-   mutable Futex m_futex;
+   mutable Futex<0> m_futex;
+   mutable Futex<1> m_yield_futex;
+   mutable Futex<50> m_sl_futex;
+   mutable std::mutex m_mutex;
 
    void SetUp() {
    }
@@ -31,6 +36,36 @@ public:
    }
 
    FutexTest(){}
+
+
+   template<class _Mutex>
+   void performance_test(_Mutex& mutex, int perfCount, int threadCount, int outOfBusyLoopCount)
+   {
+      volatile int counter = 0;
+      auto check = [&](){
+         int dummy = 1;
+         for(int i = 0; i < perfCount; ++i) {
+            // Simulate some out of the main loop operation
+            for(int j = 0; j < outOfBusyLoopCount; ++j) {
+               ++ dummy;
+            }
+            std::lock_guard guard(mutex);
+            ++counter;
+         }
+      };
+
+
+      std::vector<std::thread> threads;
+      for (int i = 0; i < threadCount; ++i) {
+         threads.emplace_back(check);
+      }
+
+      for (int i = 0; i < threadCount; ++i) {
+         threads[i].join();
+      }
+
+      EXPECT_EQ(perfCount * threadCount, counter);
+   }
 };
 
 TEST_F(FutexTest, smoke)
@@ -46,12 +81,86 @@ TEST_F(FutexTest, smoke2)
 {
    EXPECT_FALSE(m_futex.isLocked());
    {
-      std::lock_guard<Futex> guard(m_futex);
+      std::lock_guard guard(m_futex);
       EXPECT_TRUE(m_futex.isLocked());
    }
    EXPECT_FALSE(m_futex.isLocked());
 }
 
+
+TEST_F(FutexTest, thread_counter)
+{
+   performance_test(m_futex, 10000, 10, 0);
+}
+
+
+
+
+TEST_F(FutexTest, perf_test_futex_trio)
+{
+   performance_test(m_futex, 10000000, 3, 30);
+}
+
+TEST_F(FutexTest, perf_test_yield_trio)
+{
+   performance_test(m_yield_futex, 10000000, 3, 30);
+}
+
+TEST_F(FutexTest, perf_test_sl_trio)
+{
+   performance_test(m_sl_futex, 10000000, 3, 30);
+}
+
+TEST_F(FutexTest, perf_test_mutex_trio)
+{
+   performance_test(m_mutex, 10000000, 3, 30);
+}
+
+
+
+
+TEST_F(FutexTest, perf_test_futex_contention)
+{
+   performance_test(m_futex, 10000000, 4, 0);
+}
+
+TEST_F(FutexTest, perf_test_yield_contention)
+{
+   performance_test(m_yield_futex, 10000000, 4, 0);
+}
+
+TEST_F(FutexTest, perf_test_sl_contention)
+{
+   performance_test(m_sl_futex, 10000000, 4, 0);
+}
+
+TEST_F(FutexTest, perf_test_mutex_contention)
+{
+   performance_test(m_mutex, 10000000, 4, 0);
+}
+
+
+
+
+TEST_F(FutexTest, perf_test_futex_non_contention)
+{
+   performance_test(m_futex, 1000000, 10, 1000);
+}
+
+TEST_F(FutexTest, perf_test_yield_non_contention)
+{
+   performance_test(m_yield_futex, 1000000, 10, 1000);
+}
+
+TEST_F(FutexTest, perf_test_sl_non_contention)
+{
+   performance_test(m_sl_futex, 1000000, 10, 1000);
+}
+
+TEST_F(FutexTest, perf_test_mutex_non_contention)
+{
+   performance_test(m_mutex, 1000000, 10, 1000);
+}
 FALCON_TEST_MAIN
 
 /* end of futex.fut.cpp */
